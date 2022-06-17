ba0b7ca1a78e680db54efc193a75ab0d436eea1a
skipancho <ctg1770a@gmail.com>
2022-06-17 14:41:50 +0900

    Initial commit

---
+++ app/src/main/java/com/jjsh/gpsexample/App.kt
@@ -0,0 +1,4 @@
+package com.jjsh.gpsexample
+
+class App {
+}
---
+++ app/src/main/java/com/jjsh/gpsexample/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jjsh.gpsexample.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jjsh.gpsexample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
