c4b4d3e725ac9caaaea5601a8ad206fb4d868abc
Skipancho <79133730+Skipancho@users.noreply.github.com>
2022-07-01 17:54:00 +0900

    Update App.kt

--- app/src/main/java/com/jjsh/gpsexample/App.kt
+++ app/src/main/java/com/jjsh/gpsexample/App.kt
@@ -13,6 +13,7 @@ class App : Application() {
     companion object{
         val stationData = mutableListOf<SubwayStation>()
         val progressOn = MutableLiveData(false)
+        val selectedPosition = doubleArrayOf(0.0,0.0)
         lateinit var instance : App
     }
 }
