511ad28cfa3f7bda14df0e9ecb8077a620a236f6
skipancho <ctg1770a@gmail.com>
2022-07-02 15:53:18 +0900

    naver map sdk 등록

--- app/src/main/java/com/jjsh/gpsexample/App.kt
+++ app/src/main/java/com/jjsh/gpsexample/App.kt
@@ -1,14 +1,16 @@
 package com.jjsh.gpsexample
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import com.jjsh.gpsexample.datas.SubwayStation
+import com.naver.maps.map.NaverMapSdk
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(getString(R.string.naver_map_client_id))
+
     }
     companion object{
         val stationData = mutableListOf<SubwayStation>()
